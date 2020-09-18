30e9891fd33295e495b12da449d7aba46bb50d85
shinjonathan <shin@greenstreetdigital.com>
2020-09-18 13:36:07 -0700

    Adds react search-ui

--- src/pages/ProjectSearch/projectSearch.js
+++ src/pages/ProjectSearch/projectSearch.js
@@ -1,5 +1,19 @@
-import React from 'react'
-import { Orbitals} from 'react-spinners-css';
+import React from 'react';
+import { Orbitals } from 'react-spinners-css';
+
+
+import { Results, SearchBox, SearchProvider } from "@elastic/react-search-ui";
+import { Layout } from "@elastic/react-search-ui-views";
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
+import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
+
+
+
+const connector = new AppSearchAPIConnector({
+  searchKey: "search-371auk61r2bwqtdzocdgutmg",
+  engineName: "search-ui-examples",
+  hostIdentifier: "host-2376rb"
+});
 
 
 
@@ -12,7 +26,7 @@ const ProjectSearch = () => {
   };
 
   const Content = {
-    alignContent:'center',
+    alignContent: 'center',
     verticalAlign: 'middle',
     position: 'absolute',
     margin: '0',
@@ -21,12 +35,28 @@ const ProjectSearch = () => {
     // marginRight: '-50%',
     transform: 'translate(-50%, -50%)'
   }
-  
+
 
   return (
-    <div style={ViewContainer}>
-      <span style={Content}><Orbitals color="#0CB2E7" /><p>Coming Soon...</p></span>
-    </div>
+
+
+    <SearchProvider
+      config={{
+        apiConnector: connector
+      }}
+    >
+      <div className="App">
+      {/* <div style={ViewContainer}>
+          <span style={Content}><Orbitals color="#0CB2E7" /><p>Coming Soon...</p></span>
+      </div> */}
+        <Layout
+          header={<SearchBox />}
+          bodyContent={<Results titleField="title" urlField="nps_link" />}
+        />
+      </div>
+    </SearchProvider>
+
+
   )
 }
 
